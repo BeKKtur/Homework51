16e23e3f9084e1e3a6834141ca72fc5b7c1673e6
Bektur Batyrbekov <batyrbekovbektur0@gmail.com>
2023-12-21 21:06:57 +0600

    Added completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import './App.css'
 import Person from "./Person/Person";
+import {useState} from "react";
 
-const Button = () => {
-  return(
-      <>
-        <button className="button">New numbers</button>
-      </>
-  )
+
+interface Numbers {
+    num1: number;
+    num2: number;
+    num3: number;
+    num4: number;
+    num5: number
 }
 
+
+
 function App() {
+    const [numbers, setNumber] = useState<Numbers[]>([
+        {num1: 5, num2: 11, num3: 16, num4: 23, num5: 32}
+    ])
+
+    const ChangeNumber = () => {
+        const random1:number = Math.floor(Math.random() * 4) + 5;
+        const random2:number = Math.floor(Math.random() * 5) + 10;
+        const random3:number = Math.floor(Math.random() * 4) + 16;
+        const random4:number = Math.floor(Math.random() * 8) + 21;
+        const random5:number = Math.floor(Math.random() * 6) + 30;
+        setNumber([
+            {num1: random1, num2: random2, num3: random3, num4: random4, num5: random5}
+
+        ])
+    }
   return (
-    <div>
-        <Button/>
-        <div>
-            <Person num1={5} num2={11} num3={16} num4={23} num5={32}/>
-        </div>
-    </div>
+      <div>
+          <button className="button" onClick={ChangeNumber}>New numbers</button>
+          <div>
+              <Person num1={numbers[0].num1} num2={numbers[0].num2} num3={numbers[0].num3} num4={numbers[0].num4} num5={numbers[0].num5}/>
+          </div>
+      </div>
   )
 }
 
